ea7b0b5bfc071cd62486d33d4343834181032785
djanov <danielmrdjanov@gmail.com>
2016-02-29 19:01:21 +0100

    04_11-An Aside: Dependecny Injection Parameters: whenever we see something surronded by two precent signs in a config file, its a parametr. Parameters are like variables: you set them somewhere and then use them with this syntax. We can make our own. Adding a new paramters key anywhere in the file, we can create a new parameter example our_assets_version and set it to the assets_varison what we have set below, now we need only to change that to our assets parameter: %our_assets_version% and we are redy to go, we can acces parameters from a controller using **$this->container->getParameter("the parameter name goes here")**

--- src/Yoda/EventBundle/Controller/DefaultController.php
+++ src/Yoda/EventBundle/Controller/DefaultController.php
@@ -7,6 +7,10 @@ class DefaultController extends Controller
 {
   public function indexAction($count, $firstName)
   {
+
+    //get a parameter!
+    //$this->container->getParameter('our_assets_version');
+
     // these 2 lines are equivalent
     // $em = $this->container->get('doctrine')->getManager();
     $em = $this->getDoctrine()->getManager();
